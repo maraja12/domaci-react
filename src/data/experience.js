0f65deff239fe11c5ad62be29a85ebc78f17b195
Marija Rajic <marijarajic64@gmail.com>
2023-01-29 21:33:19 +0100

    Dodat tekst koji se koristi za stranicu Experience

---
+++ src/data/experience.js
@@ -0,0 +1,39 @@
+export const jobs = [
+    {
+      'id': 1,
+      'order': 3,
+      'title': 'Full Stack Web Developer',
+      'dates': 'December 2020 - Present',
+      'duties': [
+        'Become a full stack developer, it is a good experience having a stack of technologies at once. It is not as easy as we say, but it becomes easier and having a good experience when you get more familiar with both Client and Server, then it becomes a pleasant experience of it. But to get success as a full stack developer, you need to practice more, and more, again more.',
+        'Being a full stack developer has been one of the best decisions of my life so far. I have been a full stack developer for about two years now and I am satisfactorily earning more than the average salaries compared to my peers.',
+        'Having made a lot of bad choices earlier, I judged my decision since I was not very skilled in programming and yet I decided to go for software development. The sole reason to do so was that I was intrigued by the tech world and how they are completely changing the ways we live. Imagine a day without WhatsApp or not finding the website you’re looking for online…how much we would miss out on had we not had developers to come up with creative solutions and attractive platforms.',
+        'I joined Microsoft after all my research and that turned out to be the best choice. With Microsoft, I learned the basics of programming and then moved to learn in-depth about it. They were also very adamantly focused on teaching the core principles and concepts of programming which is actually the right way to go about it and I realised that when I joined a company after getting placed through Geekster.',
+    ],
+      'company': 'MICROSOFT',
+    },
+    {
+      'id': 2,
+      'order': 2,
+      'title': 'Front-End Engineer',
+      'dates': 'May 2015 - September 2019',
+      'duties': [
+        'Hi, I want to share a little story about my experience as a Frontend Engineer for the past 4 years (from 2015). If I think back, today’s frontend is very different from when I first worked on it. Previously HTML CSS with a little jQuery was enough, now.',
+        'Bootstrap as one of the CSS framework, helped me vary much to complete the project. I don’t use it raw like most people who don’t understand CSS. But I modified it and adapted it to the existing design themes. The coolest thing in bootstrapping is the grid, where layout is very easy. Not only that, the grid also supports responsive. In bootstrap I learned a lot about how to make reusable css classes and good layout concepts in css.',
+      ],
+      'company': 'BIGDROP',
+    },
+    {
+      'id': 3,
+      'order': 1,
+      'title': 'Engineering Intern',
+      'dates': 'September 2019 - December 2020',
+      'duties': [
+        'After experiencing full-time work, I’ve come to realize that work isn’t just work. While the projects that you complete at your job are important, it’s the people in your department and on your team that leave the most lasting impacts on your life and your career. As a result, it’s really imperative to make sure that you feel compatible with the environment, the people, and the general atmosphere of your workplace.',
+        'As students, we’ve all asked ourselves the age-old question: “When am I ever going to need this in the real world?”, most often during an exam cram session or a particularly difficult homework problem. This summer, I’ve found that we do need those things in the real world – just in a different capacity.',
+        'Working in industry showed me the exact opposite – so many of the people that I met while working switched into different teams and even professions (someone switched from engineering into marketing!), and I feel much more confident in pursuing my interests after college knowing that I’ll still have the chance to explore if I so choose.'
+      ],
+      'company': 'LEVI9',
+    },
+  ];
+  
