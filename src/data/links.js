af2f503a1baeb70a2644a6f4a4740b0fabe68368
Marija Rajic <marijarajic64@gmail.com>
2023-01-29 16:10:51 +0100

    Popunjen links fajl

---
+++ src/data/links.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'experience',
+  },
+  {
+    id: 2,
+    url: '/faq',
+    text: 'FAQ',
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.facebook.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com',
+    icon: <FaLinkedin />,
+  },
+];
