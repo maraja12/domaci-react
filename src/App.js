d73a64d46b67592ed86f200274a2a8db3db26762
Marija Rajic <marijarajic64@gmail.com>
2023-01-29 22:09:48 +0100

    Dodata nova ruta Exception

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
+import Experience from './pages/Experience';
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<Experience />} />
+      </Routes>
+    </Router>
+  
+    
   );
 }
 
