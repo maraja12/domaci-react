2342ca3aa1ba589e3367e2637a88a17ed0e61f1d
Marija Rajic <marijarajic64@gmail.com>
2023-01-30 16:00:14 +0100

    Dodata pitanja za FAQ

---
+++ src/data/questions.js
@@ -0,0 +1,29 @@
+const questions = [
+    {
+      id: 1,
+      title: 'How to prepare for job interview?',
+      info: 'If you want to be prepared for the interview, try thinking of specific examples of when you have proven your skills and behaviors related to the role, but most importantly — think of outcomes. What did you achieve? How was success measured? What role did you play in making that happen?',
+    },
+    {
+      id: 2,
+      title: 'How long should my resume be?',
+      info: 'It’s okay to use more than one page. That said, be sure your summaries are succinct and relevant—and that your most important information figures prominently on the first page.',
+    },
+    {
+      id: 3,
+      title: 'What can I expect from application proccess at Microsoft?',
+      info: 'As an external applicant, you will be able to create an online application, upload a resume, and track the status of your applications via the candidate portal. This initial investment of your time will result in less data entry in future applications. You will also be able to view and apply to all global external openings, update your contact information, and refer friends via your social networks. This can all be done easily via mobile device from the candidate portal as well.',
+    },
+    {
+      id: 4,
+      title: 'Does Microsoft allow flexible work schedules?',
+      info: 'Employees regularly work with their managers on a case-by-case basis to evaluate flexible work arrangements, including part-time, job-sharing, and telecommuting based on employee and business needs. If flex-work is important to you, be sure to mention it to your recruiter so they can find out if it’s possible in the role you’re pursuing.',
+    },
+    {
+      id: 5,
+      title: 'What if I need to relocate?',
+      info: 'Apply for jobs as you normally would. If a recruiter contacts you about a job, let the recruiter know about your relocation requirements. If the particular position does not have a relocation budget available, the recruiter may also be able to assist you in finding similar jobs that do.',
+    },
+  ];
+  export default questions;
+  
